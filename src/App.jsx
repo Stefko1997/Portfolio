31d9fe961078dfbb309717ac53efb66b27569aad
Stefko1997 <blackstefan1997@gmail.com>
2024-11-05 16:40:39 +0100

    finishing navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navigation from "./features/Navigation/Navigation";
+import Home from "./features/Home/Home";
+import About from "./features/About/About";
+import Works from "./features/Works/Works";
+import Contact from "./features/Contact/Contact";
 
 function App() {
   return (
     <>
-      <p>geagaegaega</p>
-      <Navigation />
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/works" element={<Works />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
